cceec1bdf7557b5ab09dbb8a0d59b64a6a772b6c
Tom Forbes <tom@tomforb.es>
2022-03-19 14:00:53 +0000

    {"name":"geeksforgeeks-java-zh","version":"2022.3.19.0","file":"geeksforgeeks-java-zh-2022.3.19.0.tar.gz","path":"geeksforgeeks-java-zh/geeksforgeeks-java-zh-2022.3.19.0.tar.gz"}

---
+++ geeksforgeeks-java-zh/geeksforgeeks-java-zh-2022.3.19.0.tar.gz/GeeksforgeeksJavaZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geeksforgeeks-java-zh
+https://github.com/apachecn/geeksforgeeks-java-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.19.0"
