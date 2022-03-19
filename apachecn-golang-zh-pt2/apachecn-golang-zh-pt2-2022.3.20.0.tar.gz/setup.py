da52603742523d9df9cf35470561efe011a78399
Tom Forbes <tom@tomforb.es>
2022-03-19 16:40:19 +0000

    {"name":"apachecn-golang-zh-pt2","version":"2022.3.20.0","file":"apachecn-golang-zh-pt2-2022.3.20.0.tar.gz","path":"apachecn-golang-zh-pt2/apachecn-golang-zh-pt2-2022.3.20.0.tar.gz"}

---
+++ apachecn-golang-zh-pt2/apachecn-golang-zh-pt2-2022.3.20.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ApachecnGolangZhPt2
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ApachecnGolangZhPt2'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="apachecn-golang-zh-pt2",
+    version=ApachecnGolangZhPt2.__version__,
+    url="https://github.com/apachecn/apachecn-golang-zh-pt2",
+    author=ApachecnGolangZhPt2.__author__,
+    author_email=ApachecnGolangZhPt2.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="ApacheCN Golang 译文集（二）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "apachecn-golang-zh-pt2=ApachecnGolangZhPt2.__main__:main",
+            "ApachecnGolangZhPt2=ApachecnGolangZhPt2.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
