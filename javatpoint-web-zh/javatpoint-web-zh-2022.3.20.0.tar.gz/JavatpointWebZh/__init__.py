4e757525c89cb5a1fd010550a9c3d2e595abaa8c
Tom Forbes <tom@tomforb.es>
2022-03-19 21:01:26 +0000

    {"name":"javatpoint-web-zh","version":"2022.3.20.0","file":"javatpoint-web-zh-2022.3.20.0.tar.gz","path":"javatpoint-web-zh/javatpoint-web-zh-2022.3.20.0.tar.gz"}

---
+++ javatpoint-web-zh/javatpoint-web-zh-2022.3.20.0.tar.gz/JavatpointWebZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javatpoint-web-zh
+https://github.com/apachecn/javatpoint-web-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.20.0"
