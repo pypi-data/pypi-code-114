0adbcdab9aba1e70f671aafc13c85a4e1e2fb1d4
Tom Forbes <tom@tomforb.es>
2022-03-19 11:51:58 +0000

    {"name":"javatpoint-dotnet-zh","version":"2022.3.19.0","file":"javatpoint-dotnet-zh-2022.3.19.0.tar.gz","path":"javatpoint-dotnet-zh/javatpoint-dotnet-zh-2022.3.19.0.tar.gz"}

---
+++ javatpoint-dotnet-zh/javatpoint-dotnet-zh-2022.3.19.0.tar.gz/JavatpointDotnetZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""javatpoint-dotnet-zh
+https://github.com/apachecn/javatpoint-dotnet-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.19.0"
