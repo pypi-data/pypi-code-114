6e1f7a04aa8716a6c0a444b87efa03e832863cb8
Tom Forbes <tom@tomforb.es>
2022-03-19 12:33:25 +0000

    {"name":"geeksforgeeks-scala-zh","version":"2022.3.19.0","file":"geeksforgeeks_scala_zh-2022.3.19.0-py3-none-any.whl","path":"geeksforgeeks-scala-zh/geeksforgeeks_scala_zh-2022.3.19.0-py3-none-any.whl"}

---
+++ geeksforgeeks-scala-zh/geeksforgeeks_scala_zh-2022.3.19.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geeksforgeeks-scala-zh
+https://github.com/apachecn/geeksforgeeks-scala-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.19.0"
