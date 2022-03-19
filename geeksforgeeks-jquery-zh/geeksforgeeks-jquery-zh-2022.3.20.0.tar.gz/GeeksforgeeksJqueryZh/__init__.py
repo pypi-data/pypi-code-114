302ca178aa38b9d7fec490cb0244dd2b5c96939b
Tom Forbes <tom@tomforb.es>
2022-03-19 19:59:48 +0000

    {"name":"geeksforgeeks-jquery-zh","version":"2022.3.20.0","file":"geeksforgeeks-jquery-zh-2022.3.20.0.tar.gz","path":"geeksforgeeks-jquery-zh/geeksforgeeks-jquery-zh-2022.3.20.0.tar.gz"}

---
+++ geeksforgeeks-jquery-zh/geeksforgeeks-jquery-zh-2022.3.20.0.tar.gz/GeeksforgeeksJqueryZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geeksforgeeks-jquery-zh
+https://github.com/apachecn/geeksforgeeks-jquery-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.20.0"
