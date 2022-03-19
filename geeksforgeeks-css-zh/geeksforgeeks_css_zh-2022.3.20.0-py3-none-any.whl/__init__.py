40aeca871be1d23362d74ed154f8e94560846478
Tom Forbes <tom@tomforb.es>
2022-03-19 20:07:23 +0000

    {"name":"geeksforgeeks-css-zh","version":"2022.3.20.0","file":"geeksforgeeks_css_zh-2022.3.20.0-py3-none-any.whl","path":"geeksforgeeks-css-zh/geeksforgeeks_css_zh-2022.3.20.0-py3-none-any.whl"}

---
+++ geeksforgeeks-css-zh/geeksforgeeks_css_zh-2022.3.20.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geeksforgeeks-css-zh
+https://github.com/apachecn/geeksforgeeks-css-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.20.0"
