6546b3e92a5161df88277a04e210e21b8de67e73
Tom Forbes <tom@tomforb.es>
2022-03-19 19:12:19 +0000

    {"name":"golang-by-example-2020-zh","version":"2022.3.20.0","file":"golang_by_example_2020_zh-2022.3.20.0-py3-none-any.whl","path":"golang-by-example-2020-zh/golang_by_example_2020_zh-2022.3.20.0-py3-none-any.whl"}

---
+++ golang-by-example-2020-zh/golang_by_example_2020_zh-2022.3.20.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""golang-by-example-2020-zh
+https://github.com/apachecn/golang-by-example-2020-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.20.0"
