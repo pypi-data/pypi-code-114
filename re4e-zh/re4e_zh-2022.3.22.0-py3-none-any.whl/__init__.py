45f516ec01daaa2eea6250859bfdbf61dc3cd903
Tom Forbes <tom@tomforb.es>
2022-03-21 16:00:50 +0000

    {"name":"re4e-zh","version":"2022.3.22.0","file":"re4e_zh-2022.3.22.0-py3-none-any.whl","path":"re4e-zh/re4e_zh-2022.3.22.0-py3-none-any.whl"}

---
+++ re4e-zh/re4e_zh-2022.3.22.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""re4e-zh
+https://github.com/apachecn/re4e-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.3.22.0"
